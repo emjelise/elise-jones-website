c57cd19755818036e6f2d5319e4d92dfbf237f7c
Elise <elisemj@email.sc.edu>
2022-12-08 22:54:06 -0500

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from "../components/layout"
+import Header from "../components/header"
+import topNav from "../components/topnav"
 
 const Index = () => {
   return(
     <Layout>
     <h1>This is the homepage body copy area</h1>
     </Layout>
+
   )
 }
 
