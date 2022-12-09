de105ca5a26810d680f41bf6f7b8e66c7941f337
Elise <elisemj@email.sc.edu>
2022-12-09 14:25:25 -0500

    12.9.22

---
+++ src/components/card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Card = props => {
+    <div classsname="Card">
+        <h3>Web Design</h3>
+        <p>Sub Title</p>
+    </div>
+
+}
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Layout from "../components/layout"
-
+import Card from "../components/card"
 
 const Index = () => {
   return(
     <Layout>
-    <h1>This is the homepage body copy area</h1>
+      <div className="Cards">
+      <h2>How can we help</h2>
+      <Card 
+        title="Web Design"
+        text="Sub Title"/>
+      </div>
     </Layout>
 
   )
