8a9ac4d0cf2f7e1db01c665ed597f5a25f77e0e7
Elise <elisemj@email.sc.edu>
2022-12-08 22:58:59 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from "../components/layout"
-import Header from "../components/header"
-import topNav from "../components/topnav"
+
 
 const Index = () => {
   return(
