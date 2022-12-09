5d10331f0250868a9c51cad6b3c60ec1c86ce197
Elise <elisemj@email.sc.edu>
2022-12-08 23:01:43 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./header"
 import BottomNav from "./bottomNav";
+import Topnav from "./topnav";
 
 const Layout = props => {
     return(
         <div>
             <Header />
             {props.children}
+            <Topnav />
             <BottomNav />
         </div>
     )
