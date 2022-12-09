0e5edd8216a9de56270a01520466a2cd0e999d86
Elise <elisemj@email.sc.edu>
2022-12-09 14:46:31 -0500

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -2,10 +2,12 @@ import React from 'react'
 import * as cardStyles from "./card.module.css"
 
 const Card = props => {
-    <div classsname={cardStyles.Card}>
-        <h3>{props.title}</h3>
-        <p>{props.text}</p>
-    </div>
+    return (
+        <div classsname={cardStyles.Card}>
+            <h3>{props.title}</h3>
+            <p>{props.text}</p>
+        </div>
+    )
 
 }
 export default Card
