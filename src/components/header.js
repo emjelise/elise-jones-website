1761fcf4c821dea6ec4aca0a1b1e84a40e332fc7
Elise <elisemj@email.sc.edu>
2022-12-08 21:26:37 -0500

    12.8.22

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,21 @@
 import React from 'React'
+import * as heroStyles from "./hero.module.css"
+import { navigate } from "gatsby"
 
 const header = () => {
-    return <div>This is the header</div>
+    return (
+     <header className={heroStyles.hero}>
+        <div>
 
+        </div>
+        <div className={heroStyles.heroContainer}>
+         <div className={heroStyles.heroInfo}>
+            <h1>Elise Jones</h1>
+            <p>Cybersecurity and Information Design</p>
+            <button className='heroButton' onClick={()=>{navigate("/about")}}>How We Can Help</button>
+         </div>
+        </div>
+     </header>  
+    )
 }
 export default header
